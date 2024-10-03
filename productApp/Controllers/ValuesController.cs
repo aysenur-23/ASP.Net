a3e5cc3af5cd8a4be398d0119bb36e83a9c40184
Ayşe Nur Aslan <59085587+aysenur-23@users.noreply.github.com>
2024-10-03 17:32:46 +0300

    Add files via upload
    Logging İşlemleri yapıldı

---
+++ productApp/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using productApp.Models;
+
+namespace productApp.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAllProducts()
+        {
+            var products = new List<Product>()
+            {
+                new Product() {Id=1, ProductName = "Computer"},
+                new Product() {Id=1, ProductName = "Keyboard"},
+                new Product() {Id=1, ProductName = "Mouse"}
+            };
+            _logger.LogInformation("GetAllProducts action been called");
+            return Ok(products);
+        }
+
+        [HttpPost]
+
+        public IActionResult GetAllProduct([FromBody] Product product)
+        {
+            _logger.LogWarning("Product has been created.");
+            return StatusCode(201); //created 
+        }
+
+    }
+}
---
+++ productApp/Models/product.cs
@@ -0,0 +1,8 @@
+﻿namespace productApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String? ProductName { get; set; }
+    }
+}
